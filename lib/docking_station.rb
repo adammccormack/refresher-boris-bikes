7589bb0bf6a587356beaae0628abff8ac2adcef4
Adam <adam.j.mccor@gmail.com>
2022-04-23 20:48:29 +0100

    added bike.rb and spec file

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,6 @@
 class DockingStation
 
-  
+  def release_bike
+    true
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
+  let(:docking_station) { DockingStation.new }
 
+    it { is_expected.to respond_to :release_bike }
+
+    it 'releases bike' do
+     expect(docking_station.release_bike).to eq(true)
+    end
 end
