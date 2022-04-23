9a888f185734e181d57f789232ed1c91cf8815c6
Adam <adam.j.mccor@gmail.com>
2022-04-23 23:56:43 +0100

    added tests to docking_station_spec and methods to dockingstation class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,13 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :docked_bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @docked_bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   let(:docking_station) { DockingStation.new }
@@ -8,5 +9,22 @@ describe DockingStation do
     it 'releases working bikes' do
      bike = docking_station.release_bike
      expect(bike).to be_working
+    end
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it { is_expected.to respond_to :docked_bike }
+
+    it 'docks a bike' do
+      bike = Bike.new
+
+      expect(docking_station.dock(bike)).to eq(bike)
+    end
+
+    it 'confirms if bike is docked' do
+      bike = Bike.new
+      docking_station.dock(bike)
+
+      expect(docking_station.docked_bike).to eq(bike)
     end
 end
