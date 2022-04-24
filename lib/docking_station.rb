997683e968341ef2013df048d8b67cbb873af77c
Adam <adam.j.mccor@gmail.com>
2022-04-24 14:01:56 +0100

    added dock and release_bike methods to docking_station.rb added related specs

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,13 @@ class DockingStation
 
   attr_reader :docked_bike
 
-  def release_bike
-    Bike.new
-  end
-
   def dock(bike)
+    fail 'Sorry all full : (' if @docked_bike
     @docked_bike = bike
+  end
+  
+  def release_bike
+    fail 'Sorry no bikes left : (' unless @docked_bike
+    @docked_bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,25 +6,46 @@ describe DockingStation do
 
     it { is_expected.to respond_to :release_bike }
 
-    it 'releases working bikes' do
-     bike = docking_station.release_bike
-     expect(bike).to be_working
-    end
-
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it { is_expected.to respond_to :docked_bike }
+       
+    it 'raises error when no bikes available' do
+      docking_station
 
+      expect{docking_station.release_bike}.to raise_error('Sorry no bikes left : (')
+    end
+
+    it 'raises an error when at capacity' do
+      bike = Bike.new
+      docking_station.dock(bike)
+
+      expect { docking_station.dock bike }.to raise_error('Sorry all full : (')
+    end
+
+  describe '.dock' do
     it 'docks a bike' do
       bike = Bike.new
 
       expect(docking_station.dock(bike)).to eq(bike)
     end
+  end
 
+  describe '.docked_bike' do
     it 'confirms if bike is docked' do
       bike = Bike.new
       docking_station.dock(bike)
 
       expect(docking_station.docked_bike).to eq(bike)
     end
+  end
+
+  describe '.release' do
+    it 'releases a bike' do
+      bike = Bike.new
+      docking_station.dock(bike)
+
+      expect(docking_station.release_bike).to eq(bike)
+    end
+  end
 end
