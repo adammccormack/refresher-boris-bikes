f9607f06f44279f289e194e0187d501fddf896e7
Adam <adam.j.mccor@gmail.com>
2022-06-20 17:31:26 +0100

    1, refactored docking station and spec

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,18 +2,30 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :docked_bikes
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @docked_bikes = []
   end
 
   def dock(bike)
-    fail 'Sorry all full : (' if @docked_bikes.count >= 20
+    fail 'Sorry all full : (' if full?
     @docked_bikes << bike
   end
 
   def release_bike
-    fail 'Sorry no bikes left : (' if @docked_bikes.empty?
+    fail 'Sorry no bikes left : (' if empty?
     @docked_bikes.pop
   end
+
+  private
+
+  def full?
+    @docked_bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @docked_bikes.empty?
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,7 +18,7 @@ describe DockingStation do
 
     it 'raises an error when at capacity' do
       docking_station
-      20.times { docking_station.dock(Bike.new)}
+      DockingStation::DEFAULT_CAPACITY.times { docking_station.dock(Bike.new)}
 
       expect { docking_station.dock(Bike.new) }.to raise_error('Sorry all full : (')
     end
